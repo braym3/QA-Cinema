0084af8b8ff822c643ee50fd0fb3237bc7eaaf2e
SeanHarrisQA <sharris3@qa.com>
2023-05-09 12:34:55 +0100

    Testing git

--- cinema/final_project/src/Components/Discussion/Discussion.jsx
+++ cinema/final_project/src/Components/Discussion/Discussion.jsx
@@ -1,7 +1,41 @@
+import "./Discussion.css";
+import comments from "./comments.json";
+
 const Discussion = () => {
+  const printData = () =>
+    comments.map((data, index) => {
+      const { email, body } = data;
+      return (
+        <tr key={index}>
+          <td>{email}</td>
+          <td>{body}</td>
+          <td>
+            <form>
+              <input id={index.toString} type="submit" value="Reply" />
+            </form>
+          </td>
+        </tr>
+      );
+    });
   return (
     <>
-      <h1>Discussion Board</h1>
+      <div class="discussion">
+        <h1>Discussion Board</h1>
+      </div>
+      <div class="discussion">
+        <h1>This is the subject of the first discussion</h1>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>User</th>
+                <th>Comment</th>
+              </tr>
+            </thead>
+            <tbody>{printData()}</tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 };
