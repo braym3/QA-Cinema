6315df5d16d783996919b50eef7176658577f588
braym3 <60962768+mbray3@users.noreply.github.com>
2023-05-10 16:31:08 +0100

    find us into dev

--- cinema/final_project/src/App.js
+++ cinema/final_project/src/App.js
@@ -13,8 +13,6 @@ import PurchaseTickets from "./Components/Listings/PurchaseTickets";
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
     <Navi/>
     <div>
       <Routes>
@@ -24,32 +22,12 @@ const App = () => {
         <Route path='info' element={<Info />} />
         <Route path="about" element={<About/>}/>
         <Route path="/findus*" element={<FindUs/>}/>
-        <Route path='purchasetickets' element={<PurchaseTickets/>}/>
+        <Route path='purchasetickets/:filmID' element={<PurchaseTickets />} />
         <Route path="*" element={<Navigate to="/"/>} /> 
       </Routes>
-=======
-=======
->>>>>>> e241a926bf3449b3a811e21b09528fbd87a179ba
-      <Navi />
-      <div>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='listings' element={<Listings />} />
-          <Route path='discussion' element={<Discussion />} />
-          <Route path='info' element={<Info />} />
-          <Route path='about' element={<About />} />
-<<<<<<< HEAD
-          <Route path='findus' element={<FindUs />} />
-          <Route path='purchasetickets/:filmID' element={<PurchaseTickets />} />
-        </Routes>
->>>>>>> 399b5973ee14a71f0695c493ff19e9ebdf883387
-=======
-          <Route path="/findus*" element={<FindUs/>}/>
-          <Route path='purchasetickets/:filmID' element={<PurchaseTickets />} />
-          <Route path="*" element={<Navigate to="/"/>} /> 
-        </Routes>
->>>>>>> e241a926bf3449b3a811e21b09528fbd87a179ba
-      </div>
+
+    </div>
+  
     </>
   );
 };
