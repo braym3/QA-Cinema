e66abc6ca1d3b576dc28768c9f8d6f82df8f5a7f
michael011299 <michaelawhtham@hotmail.co.uk>
2023-05-11 09:31:56 +0100

    changing css of listings page

--- cinema/final_project/src/Components/Listings/Listings.jsx
+++ cinema/final_project/src/Components/Listings/Listings.jsx
@@ -15,38 +15,43 @@ const Listings = () => {
   }, []);
 
   return (
-    <div id='container'>
-      {filmData.map((film) => {
-        return (
-          <Card id='cards'>
-            <div id='singlecard'>
-              <div id='data'>
-                <Card.Title>{film.title}</Card.Title>
-                <Card.Text>Synopsis: {film.description}</Card.Text>
-                <Card.Text>Rating: {film.rating}</Card.Text>
-                <Card.Text>Runtime: {film.runtime} minutes</Card.Text>
-                <Card.Text>Cast: {film.cast}</Card.Text>
+    <>
+      <h1 style={{ textAlign: "center", marginTop: "10px" }}>
+        All Cinema Listings
+      </h1>
+      <div id='container'>
+        {filmData.map((film) => {
+          return (
+            <Card id='cards'>
+              <div id='singlecard'>
+                <div id='data'>
+                  <Card.Title>{film.title}</Card.Title>
+                  <Card.Text>Synopsis: {film.description}</Card.Text>
+                  <Card.Text>Rating: {film.rating}</Card.Text>
+                  <Card.Text>Runtime: {film.runtime} minutes</Card.Text>
+                  <Card.Text>Cast: {film.cast}</Card.Text>
+                </div>
+                <div id='poster'>
+                  <img src={film.filmPoster} alt='film posters' />
+                </div>
+                <Link to={"/purchasetickets/" + film._id}>
+                  <Button
+                    className='button'
+                    style={{
+                      float: "right",
+                      marginRight: "-165px",
+                      marginTop: "1%",
+                    }}
+                  >
+                    See all Times and Purchase tickets
+                  </Button>
+                </Link>
               </div>
-              <div id='poster'>
-                <img src={film.filmPoster} alt='film posters' />
-              </div>
-              <Link to={"/purchasetickets/" + film._id}>
-                <Button
-                  className='button'
-                  style={{
-                    float: "right",
-                    marginRight: "-165px",
-                    marginTop: "1%",
-                  }}
-                >
-                  See all Times and Purchase tickets
-                </Button>
-              </Link>
-            </div>
-          </Card>
-        );
-      })}
-    </div>
+            </Card>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
