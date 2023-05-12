326af01ce8f6948fe96c1a73071a5492a9c4f284
okelly54 <okelly@qa.com>
2023-05-12 10:58:14 +0100

    images work

--- cinema/final_project/src/Components/Screens/Screens.jsx
+++ cinema/final_project/src/Components/Screens/Screens.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import '../Resources';
+import '../Screens/Screens.css'
 
 const Screens = () => {
     return (
         <>
             <div class="standard">
                 <h2>Standard Screen</h2>
-                <img src="/Resources/standardmain.jpg" id="main-standard" alt=""></img>
+                <img src="https://images.unsplash.com/photo-1617914309185-9e63b3badfca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" id="main-standard" alt=""></img>
                 <div class="images">
-                    <img src="/Resources/standard1.jpg" alt=""></img>
-                    <img src="/Resources/standard2.jpg" alt=""></img>
-                    <img src="/Resources/standard3.jpg" alt=""></img>
+                    <img src="https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt=""></img>
+                    <img src="https://images.unsplash.com/photo-1585647347384-2593bc35786b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt=""></img>
+                    <img src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2luZW1hfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt=""></img>
                 </div>
             </div>
 
             <div class="deluxe">
                 <h2>Deluxe <i>Director's Box</i> Screen</h2>
-                <img src="/Resources/deluxemain.jpg" id="main-deluxe" alt=""></img>
+                <img src="https://images.unsplash.com/photo-1643553517154-24eb7fd86437?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80" id="main-deluxe" alt=""></img>
                 <div class="images">
-                    <img src="/Resources/deluxe1.jpg" alt=""></img>
-                    <img src="/Resources/deluxe2.jpg" alt=""></img>
-                    <img src="/Resources/deluxe3.jpg" alt=""></img>
+                    <img src="https://images.unsplash.com/photo-1575819974033-21c1faf2adf0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=728&q=80" alt=""></img>
+                    <img src="https://images.unsplash.com/photo-1577382144834-8a80d92b925c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" alt=""></img>
+                    <img src="https://images.unsplash.com/photo-1585647347483-22b66260dfff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt=""></img>
                 </div>
 
             </div>
