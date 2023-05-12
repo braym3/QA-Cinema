df510443dffe0339860a46a3b457b8b1820c62d8
michael011299 <michaelawhtham@hotmail.co.uk>
2023-05-12 13:37:47 +0100

    adding spinner

--- cinema/final_project/src/Components/Listings/Listings.jsx
+++ cinema/final_project/src/Components/Listings/Listings.jsx
@@ -3,7 +3,8 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getFilms } from "../../Services/filmsCalls";
+import { getFilms } from "../../Services/filmsCalls.js";
+import Spinner from "react-bootstrap/Spinner";
 
 const Listings = () => {
   const [filmData, setFilmData] = useState([]);
@@ -14,45 +15,57 @@ const Listings = () => {
     });
   }, []);
 
-  return (
-    <>
-      <h1 style={{ textAlign: "center", marginTop: "10px" }}>
-        All Cinema Listings
-      </h1>
-      <div id='container'>
-        {filmData.map((film) => {
-          return (
-            <Card id='cards'>
-              <div id='singlecard'>
-                <div id='data'>
-                  <Card.Title>{film.title}</Card.Title>
-                  <Card.Text>Synopsis: {film.description}</Card.Text>
-                  <Card.Text>Rating: {film.rating}</Card.Text>
-                  <Card.Text>Runtime: {film.runtime} minutes</Card.Text>
-                  <Card.Text>Cast: {film.cast}</Card.Text>
+  if (filmData === undefined) {
+    return (
+      <>
+        <h1 style={{ textAlign: "center", marginTop: "10px" }}>
+          All Cinema Listings
+        </h1>
+        <Spinner animation='border' role='status'>
+          <span className='visually-hidden'>Loading...</span>
+        </Spinner>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1 style={{ textAlign: "center", marginTop: "10px" }}>
+          All Cinema Listings
+        </h1>
+        <div id='container'>
+          {filmData.map((film) => {
+            return (
+              <Card id='cards'>
+                <div id='singlecard'>
+                  <div id='data'>
+                    <Card.Title>{film.title}</Card.Title>
+                    <Card.Text>Synopsis: {film.description}</Card.Text>
+                    <Card.Text>Rating: {film.rating}</Card.Text>
+                    <Card.Text>Runtime: {film.runtime} minutes</Card.Text>
+                    <Card.Text>Cast: {film.cast}</Card.Text>
+                  </div>
+                  <div id='poster'>
+                    <img src={film.filmPoster} alt='film posters' />
+                  </div>
+                  <Link to={"/purchasetickets/" + film._id}>
+                    <Button
+                      className='button'
+                      style={{
+                        float: "right",
+                        marginRight: "-165px",
+                        marginTop: "1%",
+                      }}
+                    >
+                      See all Times and Purchase tickets
+                    </Button>
+                  </Link>
                 </div>
-                <div id='poster'>
-                  <img src={film.filmPoster} alt='film posters' />
-                </div>
-                <Link to={"/purchasetickets/" + film._id}>
-                  <Button
-                    className='button'
-                    style={{
-                      float: "right",
-                      marginRight: "-165px",
-                      marginTop: "1%",
-                    }}
-                  >
-                    See all Times and Purchase tickets
-                  </Button>
-                </Link>
-              </div>
-            </Card>
-          );
-        })}
-      </div>
-    </>
-  );
+              </Card>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 };
-
 export default Listings;
