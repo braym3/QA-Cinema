91b3d0d82edc585cc999e0808432023200b373ca
braym3 <60962768+mbray3@users.noreply.github.com>
2023-05-15 09:56:38 +0100

    Styled nav

--- cinema/final_project/src/Components/Nav/Navi.jsx
+++ cinema/final_project/src/Components/Nav/Navi.jsx
@@ -5,35 +5,6 @@ import Search from "./SearchBar/Search";
 
 const Navi = () => {
 	return (
-		// <Navbar id='color-nav' variant='dark' sticky='top'>
-		// 	<Link id='nav' to='/'>
-		// 		<Navbar.Brand id='brand' href='#home'>QA Cinema</Navbar.Brand>
-		// 	</Link>
-		// 	<Nav className='me-auto' id='color-nav'>
-		// 		<Link id='nav' className='color-nav' to='/'>
-		// 			Home
-		// 		</Link>
-		// 		<Link id='nav' to='/listings'>
-		// 			Listings
-		// 		</Link>
-		// 		<Link id='nav' to='/discussion'>
-		// 			Discussion
-		// 		</Link>
-		// 		<Link id='nav' to='/findus'>
-		// 			Find us
-		// 		</Link>
-		// 		<Link id='nav' to='/about'>
-		// 			About
-		// 		</Link>
-		// 		<Link id='nav' to='/contact'>
-		// 			Contact
-		// 		</Link>
-		// 		<Form id='nav' className="d-flex search-bar" style={{float: "right"}}>
-		// 				<Search/>
-		// 		</Form>
-		// 	</Nav>
-		// </Navbar>
-
 		<Navbar id='color-nav' expand="lg" variant='dark' sticky='top'>
 		<Container>
 			<Navbar.Brand href="/" id="">QA Cinema</Navbar.Brand>
