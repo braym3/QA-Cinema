7eb0ebaa18ef560c685d6144f995adbd414e80bf
braym3 <60962768+mbray3@users.noreply.github.com>
2023-05-17 10:05:04 +0100

    Updated package lock

---
+++ cinema/final_project/src/Components/Screens/Screens.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import '../Screens/Screens.css';
+import { getScreens } from '../../Services/screensService';
+
+const Screens = () => {
+    const [screenData, setScreenData] = useState([]);
+
+    useEffect(() => {
+        getScreens().then((screens) => {
+            setScreenData(screens);
+        });
+    }, []);
+
+    const displayScreenInfo = () => {
+        return(
+            screenData.map(screen =>
+                <Container key={screen._id}>
+
+                <h2 id="s-t">{screen.name}</h2>
+                <Row>
+                    <img src={screen.mainImage} alt={screen.mainImageAlt} id="main-standard"></img>
+                </Row>
+                <Row class="images" id="images">
+                    {screen.subImages.map(i => 
+                        <Col key={i.image}>
+                            <img src={i.image} alt={i.alt} ></img>
+                        </Col>
+                    )}
+                </Row>
+
+                <div class="separator"></div>
+
+            </Container>
+            )
+
+            
+
+        );
+    }
+    return (
+        <>
+            <Container>
+                {displayScreenInfo()}
+            </Container>
+            
+        </>
+    );
+}
+export default Screens;
