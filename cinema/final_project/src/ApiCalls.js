b2eb5891b90609d1ccad47160216810931c7e021
braym3 <60962768+mbray3@users.noreply.github.com>
2023-05-17 09:50:25 +0100

    Merge branch 'dev' of github.com:braym3/QA-Cinema into find-us

---
+++ cinema/final_project/src/ApiCalls.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const filmsAPI = axios.create({
+  baseURL: "http://localhost:4494/films/",
+});
+
+export const getFilms = async () => {
+  try {
+    const { data } = await filmsAPI.get("/getAll");
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getFilmByID = async (filmID) => {
+  try {
+    const { data } = await filmsAPI.get(`/getFilm/${filmID}`);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
