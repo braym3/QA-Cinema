7986c935594e6901f5fc31ee45a41a62902004de
braym3 <60962768+mbray3@users.noreply.github.com>
2023-05-17 12:22:13 +0100

    Added more page title tests

--- cinema/final_project/__tests__/classifications_test.js
+++ cinema/final_project/__tests__/classifications_test.js
@@ -3,7 +3,7 @@ const { title } = require("process");
 const webdriver = require("selenium-webdriver");
 
 
-const serverUri = "http://localhost:3000/findus/places-to-go"; // App Server
+const serverUri = "http://localhost:3000/findus/classifications"; // App Server
 
 /**
  * Config for Chrome browser
@@ -16,28 +16,28 @@ let browser = new webdriver.Builder()
 
 
 
-// gets the about page subtitle
-function getPlacesToGoTitle() {
+// gets the classifications page header
+function getClassificationsHeader() {
 	return new Promise((resolve, reject) => {
-		let titleElem = browser.findElement({id: 'places-title'});
+		let titleElem = browser.findElement({id: 'classifications-title'});
 		let titleText = titleElem.getText();
-		titleText.then(function(placesTitle) {
-			resolve(placesTitle);
+		titleText.then(function(classificationsTitle) {
+			resolve(classificationsTitle);
 		})
 	})
 }
 
-describe("Places To Go Page", function() {
+describe("Classifications Page", function() {
 	this.timeout(5000);
 
-	// check the places to go page title
-	it("Should check that the title on the places to go page is correct", function() {
+	// check the classifications page title
+	it("Should check that the title on the classifications page is correct", function() {
 		return new Promise((resolve, reject) => {
 		browser
 		.get(serverUri)
-		.then(getPlacesToGoTitle)
+		.then(getClassificationsHeader)
 		.then(title => {
-		assert.strictEqual(title, "Places To Go");
+		assert.strictEqual(title, "Film Classifications");
 		resolve();
 		})
 		.catch(err => reject(err));
