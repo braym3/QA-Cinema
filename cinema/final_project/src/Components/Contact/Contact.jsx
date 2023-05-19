25f0568d51397772734929cb5cd7712738df0305
SeanHarrisQA <sharris3@qa.com>
2023-05-19 11:17:37 +0100

    update names

--- cinema/final_project/src/Components/Contact/Contact.jsx
+++ cinema/final_project/src/Components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { postEmailForm } from "../../Services/Contact";
+import { postEmailForm } from "../../Services/contactService";
 import "./Contact.css";
 import { Container, Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
@@ -21,58 +21,52 @@ const Contact = () => {
 
   return (
     <>
-      <Container id='contact'>
+      <Container id="contact">
         <h2>Got a question?</h2>
-        <Form
-          id='email-form'
-          action='https://formspree.io/f/mnqyrplg'
-          method='POST'
-        >
-          <Form.Group className='mb-3'>
+        <Form id="email-form" action="https://formspree.io/f/mnqyrplg" method="POST">
+          <Form.Group className="mb-3">
             <Form.Label>Email address</Form.Label>
             <Form.Control
-              id='email'
-              name='email'
-              type='email'
-              placeholder='Enter email'
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter email"
               onChange={updateState}
               required
             />
-            <Form.Text style={{ color: "white" }}>
-              We'll never share your email with anyone else.
-            </Form.Text>
+            <Form.Text style={{ color: "white" }}>We'll never share your email with anyone else.</Form.Text>
           </Form.Group>
 
-          <Form.Group className='mb-3'>
+          <Form.Group className="mb-3">
             <Form.Label>Subject</Form.Label>
             <Form.Control
-              id='subject'
-              name='subject'
-              type='text'
-              placeholder='Subject'
+              id="subject"
+              name="subject"
+              type="text"
+              placeholder="Subject"
               onChange={updateState}
               required
             />
           </Form.Group>
-          <Form.Group className='mb-3'>
+          <Form.Group className="mb-3">
             <Form.Label>Message</Form.Label>
             <Form.Control
-              id='message'
-              name='message'
-              type='text'
-              placeholder='Message'
+              id="message"
+              name="message"
+              type="text"
+              placeholder="Message"
               onChange={updateState}
               required
             />
           </Form.Group>
-          <Button type='submit' onClick={() => submitForm}>
+          <Button type="submit" onClick={() => submitForm}>
             Submit
           </Button>
         </Form>
       </Container>
-      <Container id='reachus'>
+      <Container id="reachus">
         <h3>Contact</h3>
-        <div id='grid'>
+        <div id="grid">
           <div style={{ gridCol: 1 }}>
             <h4>Address:</h4>
             <p>53 Portland St, Manchester, M1 3LD</p>
